c2a765e06ca772de97a631395a7cdf7d8119b529
gurpzsingh97 <79270339+gurpzsingh97@users.noreply.github.com>
2023-04-01 23:30:31 +0100

    rough site with just the intention to go live for now

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Girl(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    attending = models.BooleanField(default=False)
+    song_request = models.CharField(max_length=100, blank=True)
+
+
+class Boy(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    attending = models.BooleanField(default=False)
+    song_request = models.CharField(max_length=100, blank=True)
+
